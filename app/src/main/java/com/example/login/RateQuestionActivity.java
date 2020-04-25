327bd52427c36d4dc7de45680d09804389986323
alexdinisor <alexandra_dinisor@yahoo.com>
2020-04-25 22:43:32 +0300

    Add Questions list functionality in RateQuestionActivity

--- app/src/main/java/com/example/login/RateQuestionActivity.java
+++ app/src/main/java/com/example/login/RateQuestionActivity.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ListView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
@@ -43,19 +52,91 @@ public class RateQuestionActivity extends AppCompatActivity {
                 R.layout.q_suggestion_listview_row_layout);
         qListView.setAdapter(questionArrayAdapter);
 
-        List<String[]> friendList = readData();
-        for (String[] friendData : friendList) {
-            String questionName = friendData[0];
-            String rightAnswer = friendData[0];
-            String wrongAnswer1 = friendData[0];
-            String wrongAnswer2 = friendData[0];
-            String wrongAnswer3 = friendData[0];
-            String domain = friendData[1];
+//        List<String[]> friendList = readData();
+//        for (String[] friendData : friendList) {
+//            String questionName = friendData[0];
+//            String rightAnswer = friendData[0];
+//            String wrongAnswer1 = friendData[0];
+//            String wrongAnswer2 = friendData[0];
+//            String wrongAnswer3 = friendData[0];
+//            String domain = friendData[1];
+//
+//            QuestionData q = new QuestionData(questionName, rightAnswer,
+//                    wrongAnswer1, wrongAnswer2, wrongAnswer3, domain);
+//            questionArrayAdapter.add(q);
+//        }
 
-            QuestionData q = new QuestionData(questionName, rightAnswer,
-                    wrongAnswer1, wrongAnswer2, wrongAnswer3, domain);
-            questionArrayAdapter.add(q);
-        }
+        Call<ResponseBody> mService = service.get_suggested_questions();
+
+        mService.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                assert response.body() != null;
+                String suggestedQAndA = null;
+                try {
+                    suggestedQAndA = response.body().string();
+                    Log.v("FRAGEN:", suggestedQAndA);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                JSONObject listObj = null;
+                try {
+                    assert suggestedQAndA != null;
+                    listObj = new JSONObject(suggestedQAndA);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                int jsonLength = listObj.length();
+                Log.v("SIZE:", String.valueOf(jsonLength));
+
+                for (int i = 0; i < jsonLength; i++) {
+                    try {
+                        String qAItem = listObj.getString(String.valueOf(i));
+
+                        assert qAItem != null;
+                        JSONObject qaObj = new JSONObject(qAItem);
+
+                        String wrongAnswer1 = qaObj.getString("answer0");
+                        String wrongAnswer2 = qaObj.getString("answer1");
+                        String wrongAnswer3 = qaObj.getString("answer2");
+                        String rightAnswer = qaObj.getString("answer3");
+                        String domain = null;
+                        if (qaObj.getString("domain").equals("1"))
+                            domain = "Geography";
+                        else if (qaObj.getString("domain").equals("2"))
+                            domain = "History";
+                        else if (qaObj.getString("domain").equals("3"))
+                            domain = "Science";
+                        else if (qaObj.getString("domain").equals("4"))
+                            domain = "Movies & Celebrities";
+                        else if (qaObj.getString("domain").equals("5"))
+                            domain = "Arts & Sports";
+
+
+                        String question = qaObj.getString("question");
+
+                        QuestionData suggestedQ = new QuestionData(question, rightAnswer,
+                                wrongAnswer1, wrongAnswer2, wrongAnswer3, domain);
+
+                        questionArrayAdapter.insert(suggestedQ, i);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.v("FAIL User Q List View", t.getMessage());
+
+            }
+        });
     }
 
     public List<String[]> readData() {
