51b8a5f7b31d317e526ae22f637c9ded31b9aea0
alexdinisor <alexandra_dinisor@yahoo.com>
2020-05-10 20:52:34 +0300

    Remove back button in Dashboard

--- app/src/main/java/com/example/login/Dashboard.java
+++ app/src/main/java/com/example/login/Dashboard.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -211,6 +212,28 @@ public class Dashboard extends AppCompatActivity {
         }
 
 
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            //finish();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit Trivia", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 
 
